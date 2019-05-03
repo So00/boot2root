5ba1d89d79ff3423ef67f37b5fd5de41efac9ea4
Anthony TOURNERIE <atourner@e2r8p3.42.fr>
2019-05-03 10:26:54 +0200

    Teenage mutant

--- scripts/turtle.php
+++ scripts/turtle.php
@@ -1,5 +1,12 @@
 <?php
 
 	$turtle = file_get_contents("turtle");
-	$parsed = preg_replace("/^(Tourne droite de )([0-9]+).*$/ig", "right $2", $turtle);
-	echo $parsed."\n";
+	while (preg_match("/^(Tourne droite de )([0-9]+).*$/im", $turtle))
+		$turtle = preg_replace("/^(Tourne droite de )([0-9]+).*$/im", "right $2", $turtle);
+	while (preg_match("/^(Avance )([0-9]+).*$/im", $turtle))
+		$turtle = preg_replace("/^(Avance )([0-9]+).*$/im", "forward $2", $turtle);
+	while (preg_match("/^(Recule )([0-9]+).*$/im", $turtle))
+		$turtle = preg_replace("/^(Recule )([0-9]+).*$/im", "back $2", $turtle);
+	while (preg_match("/^(Tourne gauche de )([0-9]+).*$/im", $turtle))
+		$turtle = preg_replace("/^(Tourne gauche de )([0-9]+).*$/im", "left $2", $turtle);
+	file_put_contents("turtleSolver", $turtle);
